216a58c17554ed4d012b09d0af63ebd36cd0b7ad
Yumi Chen <27608365+YumiChen@users.noreply.github.com>
2024-01-28 03:21:20 -0400

    Add displaying books, add book button and popups in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
+"use client";
+
+import { selectBooks, selectError } from "@/lib/features/books/booksSlice";
+
 import AddBookButton from "./AddBookButton";
+import AddBookPopup from "./AddBookPopup";
+import Book from "./Book";
+import EditBookPopup from "./EditBookPopup";
+import { useAppSelector } from "@/lib/hook";
 
 export default function Home() {
+  const books = useAppSelector(selectBooks);
+  const error = useAppSelector(selectError);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex w-full min-h-screen flex-col items-center justify-between px-12 md:px-24 py-12 gap-8">
+      <h1>Book Store</h1>
+      <h2>{"Check the list of books"}</h2>
       <AddBookButton></AddBookButton>
+      {
+        !error && <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-4">{
+          books.map((book)=>(<Book key={book.id} {...book} />))
+        }</div>
+      }
+      {
+        error && <div className="w-full">
+          <p>{error}</p>
+        </div>
+      }
+      <AddBookPopup />
+      <EditBookPopup />
     </main>
   );
 }
