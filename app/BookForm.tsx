ce47b15a3e6966afd8953201a0408101a82cc532
Yumi Chen <27608365+YumiChen@users.noreply.github.com>
2024-01-28 16:43:11 -0400

    Add form validation

--- app/BookForm.tsx
+++ app/BookForm.tsx
@@ -1,6 +1,6 @@
 import { BookCategoryKey, BookType } from "@/lib/features/books/booksSlice";
 import { MouseEventHandler, useCallback } from "react";
-import { selectEditingBook, setCategory, setDescription, setErrors, setName, setPrice } from "@/lib/features/books/editingBookSlice";
+import { selectEditingBook, selectErrors, setCategory, setDescription, setErrors, setName, setPrice } from "@/lib/features/books/editingBookSlice";
 import { useAppDispatch, useAppSelector } from "@/lib/hook";
 
 import { bookCategories } from "@/utils/consts";
@@ -8,6 +8,7 @@ import { bookCategories } from "@/utils/consts";
 const bookFields: Array<keyof BookType> = ['id', 'name', 'price', 'category', 'description'];
 export default function BookForm({ onSubmit }: { onSubmit: (BookType: BookType) => void;}) {
   const editingBook = useAppSelector(selectEditingBook);
+  const errors = useAppSelector(selectErrors);
   const dispatch = useAppDispatch();
   const validateForm = useCallback((fieldName: keyof BookType, value: BookType[keyof BookType] | undefined) => {
     let isValid = true;
@@ -72,34 +73,63 @@ export default function BookForm({ onSubmit }: { onSubmit: (BookType: BookType) 
 
   return (
     <div>
+        {errors['id'] && <div className="label">
+            <span className="text-xs text-red-500">{errors['id']}</span>
+        </div>}
         <label className="form-control w-full max-w-xs">
             <div className="label">
                 <span className="label-text">BookType Name</span>
             </div>
-            <input type="text" value={editingBook.name} placeholder="Input name..." className="input input-bordered w-full max-w-xs" onChange={(event) => (dispatch(setName(event.target.value)))}/>
+            <input type="text" value={editingBook.name} placeholder="Input name..." className="input input-bordered w-full max-w-xs" onChange={(event) => {
+                dispatch(setName(event.target.value));
+                validateForm('name', event.target.value);
+            }}/>
+            <div className="label">
+                <span className="text-xs text-red-500">{errors['name']}</span>
+            </div>
         </label>
         <label className="form-control w-full max-w-xs">
             <div className="label">
                 <span className="label-text">BookType Price</span>
             </div>
-            <input type="number" value={editingBook.price} placeholder="Input price..." className="input input-bordered w-full max-w-xs" onChange={(event) => (dispatch(setPrice(parseFloat(event.target.value))))}/>
+            <input type="number" value={editingBook.price} placeholder="Input price..." className="input input-bordered w-full max-w-xs" onChange={(event) => {
+                const newPrice = parseFloat(event.target.value);
+                dispatch(setPrice(newPrice));
+                validateForm('price', newPrice);
+            }}/>
+            <div className="label">
+                <span className="text-xs text-red-500">{errors['price']}</span>
+            </div>
         </label>
         <label className="form-control w-full max-w-xs">
             <div className="label">
                 <span className="label-text">BookType Category</span>
             </div>
-            <select className="select select-bordered" value={editingBook.category || -1} onChange={(event) => (dispatch(setCategory(parseInt(event.target.value) as BookCategoryKey)))}>
+            <select className="select select-bordered" value={editingBook.category || -1} onChange={(event) => {
+                const newCategory = parseInt(event.target.value);
+                dispatch(setCategory(newCategory as BookCategoryKey));
+                validateForm('category', newCategory);
+            }}>
                 <option disabled value={-1}>Pick one</option>
                 {Object.entries(bookCategories).map(([id, category]) => {
                     return (<option key={id} value={id}>{category}</option>);
                 })}
             </select>
+            <div className="label">
+                <span className="text-xs text-red-500">{errors['category']}</span>
+            </div>
         </label>
         <label className="form-control">
             <div className="label">
                 <span className="label-text">Description</span>
             </div>
-            <textarea className="textarea textarea-bordered h-24" value={editingBook.description} placeholder="Input description..." onChange={(event) => (dispatch(setDescription(event.target.value)))}></textarea>
+            <textarea className="textarea textarea-bordered h-24" value={editingBook.description} placeholder="Input description..." onChange={(event) => {
+                dispatch(setDescription(event.target.value));
+                validateForm('description', event.target.value);
+            }}></textarea>
+            <div className="label">
+                <span className="text-xs text-red-500">{errors['description']}</span>
+            </div>
         </label>
         <button className="btn mt-8 btn-accent" onClick={onSubmitForm}>Submit</button>
     </div>
