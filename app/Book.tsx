c8391eb23d78aababcac45dd4803d3da839535a0
Yumi Chen <27608365+YumiChen@users.noreply.github.com>
2024-01-28 03:20:20 -0400

    Add Book component

--- app/Book.tsx
+++ app/Book.tsx
@@ -1,7 +1,32 @@
-export default function Book() {
+import { BookType, removeBook } from "@/lib/features/books/booksSlice";
+
+import { MouseEventHandler } from "react";
+import { bookCategories } from "@/utils/consts";
+import { setEditingBook } from "@/lib/features/books/editingBookSlice";
+import { useAppDispatch } from "@/lib/hook";
+
+export default function Book({ id, name, price, category, description } : BookType) {
+  const dispatch = useAppDispatch();
+
+  const openEditBookPopup = () => {
+    dispatch(setEditingBook({ id, name, price, category, description }));
+    (document.querySelector("#edit_book_modal") as HTMLDialogElement)?.showModal();
+  };
+
+  const removeTheBook: MouseEventHandler<HTMLButtonElement> = () => {
+    dispatch(removeBook(id));
+  };
+
   return (
-    <div>
-        <div>Book title</div>
+    <div className="w-full">
+      <div className="card lg:card-side shadow-xl glass cursor-pointer" onClick={openEditBookPopup}>
+        <div className="card-body min-h-[212px]">
+          <h3 className="card-title ellipsis-2">{name}</h3>
+              <p>{`$${price} CAD`}</p>
+              <p className="ellipsis-1">{bookCategories[category]}</p>
+        </div>
+      </div>
+      <button className="btn w-full mt-3" onClick={removeTheBook}>Remove</button>
     </div>
   );
 }
