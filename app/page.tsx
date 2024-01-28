0e34e514593031a49a2f92608faf3edecbde87b8
Yumi Chen <27608365+YumiChen@users.noreply.github.com>
2024-01-28 18:39:52 -0400

    Modify error message display

--- app/page.tsx
+++ app/page.tsx
@@ -13,20 +13,22 @@ export default function Home() {
   const error = useAppSelector(selectError);
 
   return (
-    <main className="flex w-full min-h-screen flex-col items-center justify-between px-12 md:px-24 py-12 gap-8">
-      <h1>Book Store</h1>
-      <h2>{"Check the list of books"}</h2>
-      <AddBookButton></AddBookButton>
-      {
-        !error && <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-4">{
-          books.map((book)=>(<Book key={book.id} {...book} />))
-        }</div>
-      }
-      {
-        error && <div className="w-full">
-          <p>{error}</p>
-        </div>
-      }
+    <main className="min-h-screen px-12 md:px-24 py-12">
+      <div className="flex w-full flex-col items-center justify-between gap-4">
+        <h1>Book Store</h1>
+        <h2>{"Check the list of books"}</h2>
+        <AddBookButton></AddBookButton>
+        {
+          !error && <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-4">{
+            books.map((book)=>(<Book key={book.id} {...book} />))
+          }</div>
+        }
+        {
+          error && <div className="w-full p-24">
+            <p className="text-center">Something went wrong</p>
+          </div>
+        }
+      </div>
       <AddBookPopup />
       <EditBookPopup />
     </main>
